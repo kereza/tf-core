a0c9863193b334c5950a1dc77f0dbe5384c10963
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-06 20:49:01 +0200

    first commit

--- vpc/kms.tf
+++ vpc/kms.tf
@@ -1,5 +1,16 @@
 resource "aws_kms_key" "security" {
   description             = "This key is used to encrypt server side objects"
-  deletion_window_in_days = 10
+  deletion_window_in_days = 7
   enable_key_rotation     = true
+}
+
+resource "aws_kms_key" "this" {
+  description             = "KMS key for system wide encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/test"
+  target_key_id = aws_kms_key.this.key_id
 }
