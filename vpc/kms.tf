5972bcc58d1c2c837b71e1f5cc0dbcc053a11d63
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-06 20:59:54 +0200

    first commit

--- vpc/kms.tf
+++ vpc/kms.tf
@@ -1,9 +1,3 @@
-resource "aws_kms_key" "security" {
-  description             = "This key is used to encrypt server side objects"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-}
-
 resource "aws_kms_key" "this" {
   description             = "KMS key for system wide encryption"
   deletion_window_in_days = 7
