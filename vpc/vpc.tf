fce699f82d57ae4c61cc9c39e4c283dbdd6e8c6d
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-07 12:51:29 +0200

    first commit

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -3,16 +3,16 @@ module "vpc" {
   version = "3.0.0"
 
   name = var.env
-  cidr = local.per_environment_settings[var.environment]["cidr"]
+  cidr = local.per_env_settings[var.env]["cidr"]
 
-  azs             = local.per_environment_settings[var.environment]["azs"]
-  private_subnets  = local.per_environment_settings[var.environment]["private_subnets"]
-  public_subnets  = local.per_environment_settings[var.environment]["public_subnets"]
-  database_subnets = local.per_environment_settings[var.environment]["database_subnets"]
+  azs             = local.per_env_settings[var.env]["azs"]
+  private_subnets  = local.per_env_settings[var.env]["private_subnets"]
+  public_subnets  = local.per_env_settings[var.env]["public_subnets"]
+  database_subnets = local.per_env_settings[var.env]["database_subnets"]
 
   enable_dns_hostnames               = true
   enable_nat_gateway                 = true
-  single_nat_gateway                 = local.per_environment_settings[var.environment]["single_nat_gateway"]
+  single_nat_gateway                 = local.per_env_settings[var.env]["single_nat_gateway"]
   enable_dhcp_options                = true
   dhcp_options_domain_name           = "ec2.internal"
   dhcp_options_domain_name_servers   = ["AmazonProvidedDNS"]
