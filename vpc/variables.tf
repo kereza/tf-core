a8b0535b00bb1f272a02ab380ea7df04e2b77c4a
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-08 20:14:29 +0200

    work fmt

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,45 +1,45 @@
 variable "account_id" {
-    description = "Id of the AWS account to assume role. It is a TF cloud variable, coming from the master workspace"
-    type = string
+  description = "Id of the AWS account to assume role. It is a TF cloud variable, coming from the master workspace"
+  type        = string
 }
 
 variable "region" {
-    description = "The default region in which we build infrastructure. It is a TF cloud variable, coming from the master workspace"
-    type = string
+  description = "The default region in which we build infrastructure. It is a TF cloud variable, coming from the master workspace"
+  type        = string
 }
 
 variable "env" {
-    description = "The current env. It is a TF cloud variable, coming from the master workspace"
-    type = string
+  description = "The current env. It is a TF cloud variable, coming from the master workspace"
+  type        = string
 }
 
 locals {
   per_environment_settings = tomap({
     dev = {
-      cidr = "10.2.0.0/16"
-      azs = [ "eu-central-1a", "eu-central-1b"]
-      private_subnets = ["10.2.4.0/23", "10.2.68.0/23"]
-      public_subnets = ["10.2.0.0/24", "10.2.64.0/24"]
-      database_subnets = ["10.2.8.0/24", "10.2.76.0/24" ]
+      cidr               = "10.2.0.0/16"
+      azs                = ["eu-central-1a", "eu-central-1b"]
+      private_subnets    = ["10.2.4.0/23", "10.2.68.0/23"]
+      public_subnets     = ["10.2.0.0/24", "10.2.64.0/24"]
+      database_subnets   = ["10.2.8.0/24", "10.2.76.0/24"]
       single_nat_gateway = true
     }
     prod = {
-      cidr = "10.4.0.0/16"
-      azs = [ "eu-central-1a", "eu-central-1b"]
-      private_subnets = ["10.4.4.0/23", "10.4.68.0/23"]
-      public_subnets = ["10.4.0.0/24", "10.4.64.0/24"]
-      database_subnets = ["10.4.8.0/24", "10.4.76.0/24" ]
+      cidr               = "10.4.0.0/16"
+      azs                = ["eu-central-1a", "eu-central-1b"]
+      private_subnets    = ["10.4.4.0/23", "10.4.68.0/23"]
+      public_subnets     = ["10.4.0.0/24", "10.4.64.0/24"]
+      database_subnets   = ["10.4.8.0/24", "10.4.76.0/24"]
       single_nat_gateway = true
     }
   })
 
-  cidr = local.per_environment_settings[var.env]["cidr"]
-  azs = local.per_environment_settings[var.env]["azs"]
-  private_subnets = local.per_environment_settings[var.env]["private_subnets"]
-  public_subnets = local.per_environment_settings[var.env]["public_subnets"]
-  database_subnets = local.per_environment_settings[var.env]["database_subnets"]
+  cidr               = local.per_environment_settings[var.env]["cidr"]
+  azs                = local.per_environment_settings[var.env]["azs"]
+  private_subnets    = local.per_environment_settings[var.env]["private_subnets"]
+  public_subnets     = local.per_environment_settings[var.env]["public_subnets"]
+  database_subnets   = local.per_environment_settings[var.env]["database_subnets"]
   single_nat_gateway = local.per_environment_settings[var.env]["single_nat_gateway"]
 
-  tags = {terraform = "true", environment = var.env}
+  tags = { terraform = "true", environment = var.env }
 }
 
