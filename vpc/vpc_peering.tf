8d8cd00a3d5711bcb96c0b25311213657ca5d31c
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-20 14:52:02 +0200

    peering reverse

--- vpc/vpc_peering.tf
+++ vpc/vpc_peering.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_peering_connection" "keri_org" {
 
 
 resource "aws_route" "private_table" {
-  count = length(module.vpc.private_route_table_ids)
+  count                     = length(module.vpc.private_route_table_ids)
   route_table_id            = module.vpc.private_route_table_ids[count.index]
   destination_cidr_block    = data.terraform_remote_state.vpn.outputs.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.keri_org.id
