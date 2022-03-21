1b5c5b59be6fa358b893a6fdfabbc771595c79ef
Ivan Kerezov <ivan.kerezov@tis.biz>
2022-03-21 21:40:49 +0200

    new env

--- vpc/ssh.tf
+++ vpc/ssh.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "default" {
   key_name   = var.environment
-  public_key = templatefile("${path.module}/ssh-keys/${var.environment}.pub", {})
+  public_key = templatefile("${path.module}/ssh-keys/${var.env}.pub", {})
 }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
+  version = "3.11.0"
 
   name = var.env
   cidr = local.cidr
