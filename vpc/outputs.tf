d425f2e908f2f6db319954ff9b0480f16e0b7dfe
Ivan Kerezov <ivan.kerezov@tis.biz>
2022-04-03 10:33:18 +0200

    fix

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -16,10 +16,10 @@ output "env" {
   value       = var.env
 }
 
-output "peering_id" {
-  description = "Peering connection ID"
-  value       = aws_vpc_peering_connection.keri_org.id
-}
+# output "peering_id" {
+#   description = "Peering connection ID"
+#   value       = aws_vpc_peering_connection.keri_org.id
+# }
 
 
 /*
